3f4f0ba59ef8a1e98be0a18bc542a2820867e6b0
songsong <dragonh0919@gmail.com>
2024-02-22 22:16:06 +0900

    Feat: 로그인 페이지 퍼블 구현

--- src/page/login/Login.jsx
+++ src/page/login/Login.jsx
@@ -4,7 +4,16 @@ import styles from "./Login.module.scss";
 const Login = () => {
   return (
     <div className={styles.container}>
-      <h1>Login</h1>
+      <div className={styles["login-box"]}>
+        <h2>SIGN IN</h2>
+        <input type="text" placeholder="ID"></input>
+        <input type="password" placeholder="PASSWORD"></input>
+        <button className={styles.signin}>SIGN IN</button>
+        <div className={styles["support-box"]}>
+          <button>forgot password?</button>
+          <button>sign up</button>
+        </div>
+      </div>
     </div>
   );
 };
