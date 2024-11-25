49afc8c605e4029ca302fa2a586ab96eea628c43
songsong <dragonh0919@gmail.com>
2024-11-26 02:47:12 +0900

    Feat: vite.config.js proxy 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/cozeans/",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://52.78.179.19:8080",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+      },
+    },
+  },
 });
