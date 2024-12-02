6b53bddd96f3f2e1fbe1f1203439f4b85bda0eed
songsong <dragonh0919@gmail.com>
2024-12-02 17:49:25 +0900

    Fix: config.ts server path 수정

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 export const ROOT_PATH = "/cozeans/";
 
 // Server path
-export const SERVER_PATH = "/api/";
+export const SERVER_PATH = "http://52.78.179.19:8080/api/";
 // export const SERVER_PATH = "http://localhost:8080/api/";
