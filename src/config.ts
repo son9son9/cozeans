9ce051ac9dd53c6b8fd5846cd5b6c809bbe76449
songsong <dragonh0919@gmail.com>
2024-12-05 04:09:08 +0900

    Fix: server path 롤백

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const ROOT_PATH = "/cozeans/";
 // CORS, Mixed Content Error 우회 URL
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 // Server path
-export const SERVER_PATH = `${CORS_PROXY}http://52.78.179.19:8080/api/`;
+export const SERVER_PATH = `http://52.78.179.19:8080/api/`;
 // export const SERVER_PATH = "http://localhost:8080/api/";
